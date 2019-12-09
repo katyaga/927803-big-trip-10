4e03ccfadd9057e78415ba88e63253a64b5ae003
user <aevakatya@gmail.com>
2019-12-10 00:04:24 +0700

    Добавлен NoPOints класс, подключен в main.js, реализовано закрытие формы редактирования при клике ESK

---
+++ src/components/no-points.js
@@ -0,0 +1,25 @@
+import {createElement} from '../utils.js';
+
+export default class NoPoints {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return (
+      `<p class="trip-events__msg">Click New Event to create your first point</p>`
+    );
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import SortComponent from "./components/sort";
 import FormEditComponent from "./components/form-edit";
 import TripDaysComponent from "./components/trip-days";
 import TripCardComponent from "./components/trip-card";
+import NoPoints from "./components/no-points";
 import {generateTripDays, getTripCost} from "./mock/trip-card";
 import {generateSiteMenu} from "./mock/site-menu";
 import {generateFilters} from "./mock/filter";
@@ -14,20 +15,36 @@ const filters = generateFilters();
 const siteMenu = generateSiteMenu();
 export const tripDays = generateTripDays();
 
-const renderCard = (card, i) => {
+const renderCard = (tripEventsListElement, card, i) => {
   const cardComponent = new TripCardComponent(card);
+
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToCard();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+  const replaceEditToCard = () => {
+    editCardComponent.getElement().parentElement.replaceChild(cardComponent.getElement(), editCardComponent.getElement());
+  };
+
+  const replaceCardToEdit = () => {
+    cardComponent.getElement().parentElement.replaceChild(editCardComponent.getElement(), cardComponent.getElement());
+  };
+
   const editCardComponent = new FormEditComponent(card);
 
   const editButton = cardComponent.getElement().querySelector(`.event__rollup-btn`);
 
   editButton.addEventListener(`click`, () => {
-    cardComponent.getElement().parentElement.replaceChild(editCardComponent.getElement(), cardComponent.getElement());
+    replaceCardToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
   });
 
   const editForm = editCardComponent.getElement().querySelector(`form`);
-  editForm.addEventListener(`submit`, () => {
-    editCardComponent.getElement().parentElement.replaceChild(cardComponent.getElement(), editCardComponent.getElement());
-  });
+  editForm.addEventListener(`submit`, replaceEditToCard);
 
   render(tripEventsListElement[i], cardComponent.getElement(), RenderPosition.BEFOREEND);
 };
@@ -36,21 +53,28 @@ const tripInfoElement = document.querySelector(`.trip-main__trip-info`);
 const tripCostElement = tripInfoElement.querySelector(`.trip-info__cost-value`);
 tripCostElement.innerHTML = getTripCost(tripDays);
 
-render(tripInfoElement, new RouteComponent(tripDays).getElement(), RenderPosition.AFTERBEGIN);
-
 const tripControlsElement = document.querySelector(`.trip-main__trip-controls`);
 const menuTitleElement = tripControlsElement.querySelector(`h2`);
 render(menuTitleElement, new SiteMenuComponent(siteMenu).getElement(), RenderPosition.AFTEREND);
 render(tripControlsElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
 
+const isTripPoints = tripDays.flat().length > 0;
 const tripEventsElement = document.querySelector(`.trip-events`);
-render(tripEventsElement, new SortComponent().getElement(), RenderPosition.BEFOREEND);
-render(tripEventsElement, new TripDaysComponent(tripDays).getElement(), RenderPosition.BEFOREEND);
 
-const tripDaysListElement = tripEventsElement.querySelector(`.trip-days`);
-const tripEventsListElement = tripDaysListElement.querySelectorAll(`.trip-events__list`);
 
-tripDays.forEach(((tripDay, i) => tripDay.forEach((tripCard) => {
-  renderCard(tripCard, i);
-})));
+if (!isTripPoints) {
+  render(tripEventsElement, new NoPoints().getElement(), RenderPosition.BEFOREEND);
+} else {
+  render(tripInfoElement, new RouteComponent(tripDays).getElement(), RenderPosition.AFTERBEGIN);
+  render(tripEventsElement, new SortComponent().getElement(), RenderPosition.BEFOREEND);
+  render(tripEventsElement, new TripDaysComponent(tripDays).getElement(), RenderPosition.BEFOREEND);
+
+  const tripDaysListElement = tripEventsElement.querySelector(`.trip-days`);
+  const tripEventsListElement = tripDaysListElement.querySelectorAll(`.trip-events__list`);
+
+  tripDays.forEach(((tripDay, i) => tripDay.forEach((tripCard) => {
+    renderCard(tripEventsListElement, tripCard, i);
+  })));
+}
+
 
