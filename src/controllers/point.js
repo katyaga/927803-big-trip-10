9002627c24a54f9dd1aa09218af3aed38d592e0f
user <aevakatya@gmail.com>
2019-12-24 17:07:58 +0700

    исправлена ошибка cинтаксиса

--- src/controllers/point.js
+++ src/controllers/point.js
@@ -1,9 +1,9 @@
 import TripCardComponent from "../components/trip-card";
 import FormEditComponent from "../components/form-edit";
 import {render, RenderPosition, remove, replace} from "../utils/render";
-import {getRandomElement} from "../utils/common";
-import {cities, eventTypes} from "../const";
-import {generateDescriptionText, generateOptionsList} from "../mock/trip-card";
+// import {getRandomElement} from "../utils/common";
+// import {cities, eventTypes} from "../const";
+// import {generateDescriptionText, generateOptionsList} from "../mock/trip-card";
 
 export const Mode = {
   ADDING: `adding`,
